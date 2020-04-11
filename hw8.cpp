c7f5cddf029b779f589a7a3c99ef31d53ac787b3
ccheng3 <ccrcheng@ucdavis.edu>
2020-04-10 18:20:03 -0700

    added a 3rd function play_game

--- hw8.cpp
+++ hw8.cpp
@@ -7,56 +7,63 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <limits>
 using namespace std;
 
 const int WINNING_SUM = 100;
 const int LARRY_MAX_NUM_ROLLS = 1;
 const int CURLY_MAX_NUM_ROLLS = 3;
-const int MOE_MAX_NUM_ROLLS = 1000;
+const int MOE_MAX_NUM_ROLLS = 10000;
 
 // function prototypes
-void Roll_Dice(int& total_sum, int num_rolls);
-
+void Roll_Dice(int& total_sum, int num_rolls, string player_name);
+void Play_Game_Until_Target_Score(const int max_sum = WINNING_SUM);
 
 // main function
 int main() {
-	//int total_sum = 0;
-	int larry_sum = 0, curly_sum = 0, moe_sum = 0;
 	
-	int player_num = 1;
-
-
 	// replicate dice rolling base behavior
-while ((larry_sum < 100) && (curly_sum < 100) && (moe_sum < 100)) {
-	switch(player_num) {
-		case 1 :
-			cout << "Larry, its your turn\n";
-			Roll_Dice(larry_sum, LARRY_MAX_NUM_ROLLS);
-			++player_num;
-			break;
-		case 2 :
-			cout << "Curly, its your turn\n";
-			Roll_Dice(curly_sum, CURLY_MAX_NUM_ROLLS);
-			++player_num;
-			break;
-		case 3 :
-			cout << "Moe, its your turn\n";
-			Roll_Dice(moe_sum, MOE_MAX_NUM_ROLLS);
-			player_num = 1;
-			break; 
-	}
-}
+	Play_Game_Until_Target_Score();
 
 	return 0;
 }
 
 // function definitions
-void Roll_Dice(int& total_sum, int num_rolls) {
+void Play_Game_Until_Target_Score(const int max_sum) {
+	int larry_sum = 0, 		// Larry's total sum 
+		 curly_sum = 0,		// Curly's total sum
+		 moe_sum = 0;			// Moe's total sum 
+	int player_num = 1;		// player indicator variable 
+
+	while ((larry_sum < max_sum) && (curly_sum < max_sum) && (moe_sum < max_sum)) {
+		switch(player_num) {
+			case 1 :
+				cout << "Larry, its your turn\n";
+				Roll_Dice(larry_sum, LARRY_MAX_NUM_ROLLS, "Larry");
+				++player_num;
+				break;
+			case 2 :
+				cout << "Curly, its your turn\n";
+				Roll_Dice(curly_sum, CURLY_MAX_NUM_ROLLS, "Curly");
+				++player_num;
+				break;
+			case 3 :
+				cout << "Moe, its your turn\n";
+				Roll_Dice(moe_sum, MOE_MAX_NUM_ROLLS, "Moe");
+				player_num = 1;
+				break; 
+
+				return;
+		}
+	}
+}
+
+
+void Roll_Dice(int& total_sum, int num_rolls, string player_name) {
 	int dice_roll_one, dice_roll_two, current_roll_sum;
 	int total_dice_roll = 0;
 
-	//while (total_sum < 100) {
 		for (int roll_number = 1; roll_number <= num_rolls; ++roll_number) {
 			dice_roll_one = (rand() % 6) + 1;
 			dice_roll_two = (rand() % 6) + 1;
@@ -89,9 +96,19 @@ void Roll_Dice(int& total_sum, int num_rolls) {
 			}
 			total_dice_roll += current_roll_sum;
 		}
+		if (!(((dice_roll_one == 1) && (dice_roll_two == 1)) || 
+						((dice_roll_one == 1) || (dice_roll_two == 1)))) {
+				
+				cout << "  That's " << total_dice_roll 
+					  << " points for your turn\n";
+			}
 		total_sum += total_dice_roll;
 		cout << "Total points = " << total_sum << endl;
-	//}
+
+		if (total_sum >= 100) {
+			cout << player_name << " won the game with "
+				  << total_sum << " points\n";
+		}
 	
 	return;
 }
